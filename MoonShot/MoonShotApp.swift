df94bb1ac2e63f0ac034e33d7254293ae600410f
Venna Bharat <v.bharath39@gmail.com>
2023-10-09 10:03:42 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by bharat venna on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
